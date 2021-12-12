6b73d9fa26181b9f7af41e452a05c23608548061
gwanduke <gwanduke@gmail.com>
2021-12-12 19:33:07 +0900

    [war-challenge] 프로젝트 생성 & 준비

---
+++ war-challenge/war-challenge/war_challengeApp.swift
@@ -0,0 +1,18 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Kim Gwanduk on 2021/12/12.
+//
+
+import SwiftUI
+
+// https://youtu.be/F2ojC6TNwws?t=3209
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
